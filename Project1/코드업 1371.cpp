f310f4e2e4dc1077735f8548245b00dd134235cd
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2023-03-15 09:02:45 +0900

    코드업 1371 풀고있음

--- Project1/코드업 1371.cpp
+++ Project1/코드업 1371.cpp
@@ -8,23 +8,24 @@ int main() {
 
 	for (int i = 0; i < n; i++)
 	{
+		cnt = n - i;
 		for (int j = 0; j < n * 2; j++)
 		{
-			if (star == 2) {
-				cout << endl;
-				continue;
+			cnt--;
+			if (cnt > 0 || i) {
+				cout << "-";
 			}
-
-			if (j - i < n || j >= n + 1) {
-				cout << " ";
+			if (cnt <= 0) {
+				if (star <= 1) {
+					cout << "*";
+					star++;
+				}
+				else {
+					j = n * 2;
+				}
 			}
-			else if (j == n || j == n + cnt)
-			{
-				cnt++;
-				cout << "*";
-				star++;
-			}
-			
 		}
+		star = 0;
+		cout << endl;
 	}
 }
